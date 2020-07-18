588b6f1f939928fb79ebe755357d4277fdf79498
Yeonho Lee <tomatoyeonho@naver.com>
2020-07-18 10:37:22 +0900

    add events for bucket fill/empty

--- src/main/java/io/github/yeonho1/event/BlockEventHandler.java
+++ src/main/java/io/github/yeonho1/event/BlockEventHandler.java
@@ -7,6 +7,8 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.player.PlayerBucketEmptyEvent;
+import org.bukkit.event.player.PlayerBucketFillEvent;
 
 public class BlockEventHandler implements Listener {
     BlockLogPlugin plugin;
@@ -28,6 +30,24 @@ public class BlockEventHandler implements Listener {
         Player player = event.getPlayer();
         Material blockType = event.getBlock().getType();
         String log = player.getDisplayName() + " broke " + blockType.toString();
+        plugin.getLogger().info(log);
+        plugin.addLog(log);
+    }
+
+    @EventHandler
+    public void onBucketFill(PlayerBucketFillEvent event) {
+        Player player = event.getPlayer();
+        Material blockType = event.getBucket();
+        String log = player.getDisplayName() + " filled " + blockType.toString();
+        plugin.getLogger().info(log);
+        plugin.addLog(log);
+    }
+
+    @EventHandler
+    public void onBucketEmpty(PlayerBucketEmptyEvent event) {
+        Player player = event.getPlayer();
+        Material blockType = event.getBucket();
+        String log = player.getDisplayName() + " emptied " + blockType.toString();
         plugin.getLogger().info(log);
         plugin.addLog(log);
     }
